1998bd5dbf91a43406b2e4d6be97bf48ec079557
Byun Huỳnh <byun.huynh@gmail.com>
2026-02-07 00:04:07 +0700

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default {
-  base: "/NhatMinh_React/",
-};
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/NhatMinh_React/" : "/",
+});
